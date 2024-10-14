3493e6ecf5a3a52157576f09fc29ce46ef92028f
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2024-10-13 22:37:42 -0400

    Finished Initialization code, starting executing vulkan commands

--- src/vk/vk_engine.cpp
+++ src/vk/vk_engine.cpp
@@ -75,8 +75,42 @@ void VulkanEngine::init_vulkan() {
     _chosenGPU = physicalDevice.physical_device;
 }
 
-void VulkanEngine::init_swapchain() {
+// Vulkan Swapchain
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height) {
+    vkb::SwapchainBuilder swapchainBuilder {_chosenGPU, _device, _surface};
 
+    _swapchainImageFormat = VK_FORMAT_B8G8R8_UNORM;
+
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+        //.use_default_format_seelction()
+        .set_desired_format(VkSurfaceFormatKHR
+            {.format = _swapchainImageFormat,.colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR})
+        // V-SYNC
+        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+        .set_desired_extent(width, height)
+        .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+        .build()
+        .value();
+
+    _swapchainExtent = vkbSwapchain.extent;
+    // store swapchain and its related images
+    _swapchain = vkbSwapchain.swapchain;
+    _swapchainImages = vkbSwapchain.get_images().value();
+    _swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::destroy_swapchain() {
+    vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+    // destroy swapchain resources
+    for (int i = 0; _swapchainImageViews.size(); i++) {
+        vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
+    }
+}
+
+
+void VulkanEngine::init_swapchain() {
+    create_swapchain(_windowsExtent.width, _windowsExtent.height);
 }
 
 void VulkanEngine::init_commands() {
@@ -120,6 +154,13 @@ void VulkanEngine::init() {
 
 void VulkanEngine::cleanup() {
     if (_isInitialized) {
+        destroy_swapchain();
+
+        vkDestroySurfaceKHR(_instance, _surface, nullptr);
+        vkDestroyDevice(_device, nullptr);
+
+        vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+
         glfwDestroyWindow(_window);
         glfwTerminate();
     }
