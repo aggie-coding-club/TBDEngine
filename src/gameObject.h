60d64345de383348d4d8b7aea1bae1ed2f0afa98
Roei Ben Porat <roei.benporat@gmail.com>
2024-11-01 17:08:57 -0500

    Created the

---
+++ src/gameObject.h
@@ -0,0 +1,24 @@
+#include <component.h>
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class gameObject: public Component {
+public:
+    string name;
+    vector<Component> components;
+    unordered_set<string> tags;
+
+    gameObject() : id(generateUniqueId()) {}
+private:
+    const int id;
+
+    static int lastId;
+
+    static int generateUniqueId() {
+        return ++lastId;
+    }
+};
+
+int gameObject::lastId = 0;
