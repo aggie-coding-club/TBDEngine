bbcfebc2145e720a655280743f41bfc23b831f2c
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2024-11-07 17:24:22 -0600

    Saving progress

---
+++ include/core/game_object.h
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+#include "components/component.h"
+class GameObject {
+public:
+    std::string name;
+    std::vector<Component> components;
+    std::unordered_set<std::string> tags;
+
+    GameObject() : id(generateUniqueId()) {}
+private:
+    const int id;
+
+    static int lastId;
+
+    static int generateUniqueId() {
+        return lastId++;
+    }
+};
+
+int GameObject::lastId = 0;
