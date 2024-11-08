ad13b8ee0a9dca43ddd722ae54522022edab87e9
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2024-11-07 23:26:03 -0600

    Can render multiple bunnies

--- include/core/game_engine.h
+++ include/core/game_engine.h
@@ -23,10 +23,31 @@ public:
         gameObjects.push_back(bunnyObj);
     }
 
+    void test_init_2()
+    {
+        size_t n = 3;
+        glm::vec3 pos[n] = {
+            {0.0f, -1.0f, 0.0f},
+            {-2.0f, -1.0f, -3.0f},
+            {2.0f, -1.0f, -3.0f}
+        };
+        for (int i = 0; i < n; i++)
+        {
+            auto bunnyObj = std::make_shared<GameObject>();
+            auto bunnyTransform = std::make_shared<Transform>(pos[i]);
+
+            bunnyObj->components.push_back(bunnyTransform);
+
+            gameObjects.push_back(bunnyObj);
+
+        }
+
+    }
+
     // Test Constructor
     GameEngine()
     {
-        test_init();
+        test_init_2();
     }
 
     const std::vector<std::shared_ptr<GameObject>>& GetGameObjects() { return gameObjects; }
--- src/render/render_engine.cpp
+++ src/render/render_engine.cpp
@@ -120,7 +120,8 @@ void RenderEngine::Display()
 		}
 
 		glm::mat4 modelMatrix(1.0f);
-		modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.2f, -1.0f, 0.0f)) * glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		// modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.2f, -1.0f, 0.0f)) * glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		modelMatrix = glm::translate(glm::mat4(1.0f), objTransform->position) * glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
     	glm::mat4 modelInverseTranspose = glm::transpose(glm::inverse(modelMatrix));
 		program.Bind();
@@ -146,7 +147,6 @@ void RenderEngine::Display()
     	}
 		glDrawArrays(GL_TRIANGLES, 0, posBuffMap[model_path].size() / 3);
 		program.Unbind();
-
 	}
 
 }
