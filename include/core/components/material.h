443841db19febf428af78b9c578478b0198d973c
Roei Ben Porat <roei.benporat@gmail.com>
2024-11-13 09:19:28 -0600

    Fixed type "shininess" typo

--- include/core/components/material.h
+++ include/core/components/material.h
@@ -7,14 +7,14 @@ class Material: public Component {
     glm::vec3 ambient;
     glm::vec3 diffuse;
     glm::vec3 specular;
-    float shinniness;
+    float shininess;
 
     public:
         Material(const glm::vec3 ambient = glm::vec3(0.1f, 0.1f, 0.1f),
                  const glm::vec3 diffuse = glm::vec3(0.8f, 0.8f, 0.8f),
                  const glm::vec3 specular = glm::vec3(0.5f, 0.5f, 0.5f),
-                 const float shinniness = 32.0f)
-            : ambient(ambient), diffuse(diffuse), specular(specular), shinniness(shinniness)
+                 const float shininess = 32.0f)
+            : ambient(ambient), diffuse(diffuse), specular(specular), shininess(shininess)
         {
             type = MATERIAL;
         }
