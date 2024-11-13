8c16f6222a08d255f2cb281d565dc1335ecd8edb
FunniNam3 <nguyen.ev03@gmail.com>
2024-11-13 13:44:39 -0600

    File Hierarchy can now select game objects

--- include/gui/Custom_Widgets.h
+++ include/gui/Custom_Widgets.h
@@ -2,7 +2,7 @@
 #include "core/game_engine.h"
 
 // Custom widgets
-void ShowFileHierarchy();
+void ShowFileHierarchy(GameEngine *gameEngine, const std::vector<std::shared_ptr<GameObject>>& Objects);
 void ShowMenuBar();
 void ShowViewport(ImVec2 window_Size);
 void ShowDetails(std::shared_ptr<GameObject> object);
