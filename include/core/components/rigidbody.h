76cf1e0847db818efec7e6a61f678a86970d109a
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2025-03-01 09:20:42 -0800

    Set up properties to start implementing gravity

--- include/core/components/rigidbody.h
+++ include/core/components/rigidbody.h
@@ -6,8 +6,32 @@
 
 class RigidBody: public Component 
 {
-    public:
-        glm::vec3 centerOfMass;
-        glm::vec3 angularVelocity;
-        bool autoCenterOfMass;
+public:
+    // properties of rigid body
+    glm::vec3 centerOfMass;
+    glm::vec3 velocity;
+    glm::vec3 angularVelocity;
+    glm::vec3 maxVelocity = {7, 7, 7};
+    glm::vec3 maxAngularVelocity = {2, 2, 2};
+    glm::vec3 gravity = {0, 0, -9.8};
+    float mass;
+
+    // flags
+    bool autoCenterOfMass = true;
+    bool detectCollisions = true;
+    bool useGravity = true;
+    // If false, no force, gravity, or collision will affect the body
+    bool isKinematics;
+
+    /* Publics functions to be implemented later
+     * AddForce()
+     * MovePosition()
+     * MoveRotation()
+     */
+    
+private:
+    // Copy Transform values here
+    glm::vec3 position;
+    glm::vec3 rotation;
+    glm::vec3 scale;
 };
